597818f4e222cd329bb30bc5aa2e7dd3d09a874a
LBWright <lwright.js@gmail.com>
2018-12-17 20:45:54 -0600

    added models and db migrations

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you shall not pass'
     DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     
